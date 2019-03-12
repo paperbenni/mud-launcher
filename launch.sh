94c0f17cbf7adf49dfe8c2111f0f8a078dd4b867
Paper Benni <paperbenni@gmail.com>
2019-03-12 19:39:10 +0100

    add password saving ability

--- launch.sh
+++ launch.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 fzf() {
-    ~/mud/fzf "@"
+    ~/mud/fzf "$@"
 }
 
 cd "$HOME/mud"
 
 MUDLINK=$(cat mudlist.txt | fzf)
-echo "#session $MUDLINK 23" >temp.tin
-./tt++ temp.tin
-rm temp.tin
+mkdir tin
+cd tin
+
+if ! [ -e "$MUDLINK.tin" ]; then
+    echo "#session ${MUDLINK%.*} $MUDLINK 23" >"$MUDLINK".tin
+    echo "creating tintin profile"
+    dialog --title "Password" \
+        --backtitle "Save your username and password" \
+        --yesno "Do you want to remember your password?" 7 60
+    EXIT="$?"
+    clear
+    if [ "$EXIT" = "0" ]; then
+        echo "username"
+        read MUDNAME
+        echo "$MUDNAME" >>"$MUDLINK.tin"
+        echo "password"
+        read MUDPASS
+        echo "$MUDPASS" | sed 's/#/\\#/g' >>"$MUDLINK.tin"
+    fi
+fi
+
+../tt++ "$MUDLINK.tin"
